b3cdc963824d5e6877dcf0a7b4eca7b0f04b32e2
PatelPratik39 <ptl.pratik717@gmail.com>
2025-04-11 16:02:05 -0500

    login componnet

--- BankLoanApp/src/app/app.config.ts
+++ BankLoanApp/src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient()]   // whenever we need to make http requests, we have to add this
 };
--- BankLoanApp/src/app/pages/login/login.component.ts
+++ BankLoanApp/src/app/pages/login/login.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  
+  showRegisterForm = signal<boolean>(false);
+  http = inject(HttpClient);
+
+
+
+  // create an object
+  customerObject : any = {
+    "userId": 0,
+    "userName": "",
+    "emailId": "",
+    "fullName": "",
+    "password": ""
+  }
+
+  // Reactive Form
+  loginForm: FormGroup = new FormGroup({
+    userName: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  // login Object
+  loginObj: any = {
+    "userName": "",
+    "password": ""
+  }
+  
+
+  changeView(){
+    this.showRegisterForm.set(!this.showRegisterForm());
+  }
+
+  onRegister(){
+    debugger;
+    console.log(this.customerObject);
+    this.http.post("https://projectapi.gerasim.in/api/BankLoan/RegisterCustomer", this.customerObject).subscribe((res:any) => {
+      debugger;
+      if (res.result) {
+        alert("Customer Register Successfully");
+      }else{
+        alert(res.message)
+      }
+    }, error =>{
+      debugger;
+      alert("Customer Registration Failed");
+      
+    })
+  }
 
 }
